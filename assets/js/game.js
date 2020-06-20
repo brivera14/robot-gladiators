c1ed33b737b17cc120257747812a3557b1ff10bf
Bryan Rivera Gonzalez <rivera.bryan1406@gmail.com>
2020-06-20 17:44:20 -0400

    Blank/null response handling for the player name prompt fixed

--- assets/js/game.js
+++ assets/js/game.js
@@ -72,8 +72,25 @@ var randomNumber = function(min, max) {
     return value;
 }
 
+// function to set name
+var getPlayerName = function() {
+    var name = "";
+
+// ***************************************
+// ADD LOOP HERE WITH PROMPT AND CONDITION
+while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+}
+
+// ***************************************
+
+console.log("Your robot's name is" + name);
+return name;
+
+}
+
 var playerInfo = {
-    name: window.prompt("What is your robot's name"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
@@ -130,7 +147,7 @@ var startGame = function() {
     for(var i = 0; i < enemyInfo.length; i++) {
         if (playerInfo.health > 0) {
             // let user know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
-            window.alert("Welcome to Robot Gladiators! Round " + (i + 1) );
+            window.alert('Welcome to Battelbots! Round ' + (i + 1) );
         
             // pick new enemy to fight based on the index of the enemyNames array
             var pickedEnemyObj = enemyInfo[i];
